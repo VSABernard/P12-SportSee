77a37ca032934ccc7b4ecaf3aea27575b8e11333
Veronica <veronica.stevania@gmail.com>
2023-01-19 12:45:16 +0100

    Create the navigations on header

---
+++ sportsee/src/components/HeaderNav/HeaderNav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import "../HeaderNav.css";
+
+const HeaderNav = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="running man logo" />
+      </div>
+      <ul className="horizontalNav">
+        <li>Accueil</li>
+        <li>Profil</li>
+        <li>Réglage</li>
+        <li>Communauté</li>
+      </ul>
+    </header>
+  )
+}
+
+export default HeaderNav
