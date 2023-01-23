5316b9a8770b6e4705b00607110bb6a05907f793
Veronica <veronica.stevania@gmail.com>
2023-01-23 18:51:42 +0100

    Add tooltip into BarChart

---
+++ sportsee/src/components/CustomTooltip/CustomTooltip.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../CustomTooltip/CustomTooltip.css'
+
+const CustomTooltip = ({ active, payload }) => {
+    if ( active && payload && payload.length ) {
+        return (
+            <div className='tooltip-activity'>
+                <p className='tooltip-activity-weight'>{`${payload[0].value}Kg`}</p>
+                <p className='tooltip-activity-calorie'>{`${payload[0].value}kCal`}</p>
+            </div>
+        )
+    }
+    return null
+}
+
+export default CustomTooltip
--- sportsee/src/components/DailyBarChart/DailyBarChart.js
+++ sportsee/src/components/DailyBarChart/DailyBarChart.js
@@ -2,6 +2,9 @@ import React, { PureComponent } from 'react'
 // import AutoSizer from 'react-virtualized-auto-sizer'
 import { BarChart, Bar, Cell, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 import PropTypes from 'prop-types'
+
+import CustomTooltip from '../CustomTooltip/CustomTooltip'
+
 import '../DailyBarChart/DailyBarChart.css'
 
 const DailyBarChart = ({sessions}) => {
@@ -12,13 +15,13 @@ const DailyBarChart = ({sessions}) => {
   })
   
   return (
-    <div style={{ width: '100%', height: 300 }}>
+    <div style={{ width: '100%', height: 350 }}>
       <BarChart width={700} height={320} data={sessions} margin={{top: 5, right: 30, left: 0, bottom: 5}}
                 wrapperStyle={{left: "0%", right: "20%",top: "0%", bottom: "0%", background: "#FBFBFB", borderRadius: "5px", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.0212249)"}}>
         <CartesianGrid strokeDasharray="3 3" horizontal={false} vertical={false} dot={false} />
         <XAxis dataKey="day" tick={{stroke:"#9B9EAC", strokeWidth:0.5}} tickLine={false} tickSize={14} padding={{bottom:10}} margin={{top: ('50% - 24px/2')}} />
         <YAxis orientation='right' tickMargin={20} tickLine={false} axisLine={false} padding={{bottom:2, top: 60}}/>
-        <Tooltip width={39} height={63} wrapperStyle={{backgroundColor:'#E60000'}} />
+        <Tooltip content={<CustomTooltip />} />
         <Legend iconType='circle' iconSize={8} verticalAlign='top' align='right' height={24} width={277} />
         <Bar dataKey="kilogram" name="Poids (kg)" fill="#282D30" barSize={10} radius= {[5, 5, 0, 0]} />
         <Bar dataKey="calories" name="Calories brulées (kCal)" fill="#E60000" barSize={10} radius= {[5, 5, 0, 0]} />               
