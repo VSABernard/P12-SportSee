972779a98bc6ffdf730b801d0d39bd05d453fe66
Veronica <veronica.stevania@gmail.com>
2023-01-23 18:04:48 +0100

    Add text on BarChart

--- sportsee/src/components/DailyBarChart/DailyBarChart.js
+++ sportsee/src/components/DailyBarChart/DailyBarChart.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
+// import AutoSizer from 'react-virtualized-auto-sizer'
 import { BarChart, Bar, Cell, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
-import PropTypes from "prop-types"
+import PropTypes from 'prop-types'
 import '../DailyBarChart/DailyBarChart.css'
 
 const DailyBarChart = ({sessions}) => {
@@ -11,18 +12,19 @@ const DailyBarChart = ({sessions}) => {
   })
   
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <BarChart height={320} width={835} data={sessions} margin={{top: 5, right: 30, left: 0, bottom: 5}}
+    <div style={{ width: '100%', height: 300 }}>
+      <BarChart width={700} height={320} data={sessions} margin={{top: 5, right: 30, left: 0, bottom: 5}}
                 wrapperStyle={{left: "0%", right: "20%",top: "0%", bottom: "0%", background: "#FBFBFB", borderRadius: "5px", boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.0212249)"}}>
         <CartesianGrid strokeDasharray="3 3" horizontal={false} vertical={false} dot={false} />
         <XAxis dataKey="day" tick={{stroke:"#9B9EAC", strokeWidth:0.5}} tickLine={false} tickSize={14} padding={{bottom:10}} margin={{top: ('50% - 24px/2')}} />
-        <YAxis orientation='right' tickMargin={40} tickLine={false} axisLine={false} padding={{bottom:2}}/>
-        <Tooltip />
-        <Legend iconType='circle' iconSize={8} verticalAlign='top' align='right' height={24} />
-        <Bar dataKey="kilogram" name="Poids (kg)" fill="#282D30" barSize={7} borderRadius= "5px 5px 0px 0px" />
-        <Bar dataKey="calories" name="Calories brulées (kCal)" fill="#E60000" barSize={7} borderRadius= "5px 5px 0px 0px" />            
+        <YAxis orientation='right' tickMargin={20} tickLine={false} axisLine={false} padding={{bottom:2, top: 60}}/>
+        <Tooltip width={39} height={63} wrapperStyle={{backgroundColor:'#E60000'}} />
+        <Legend iconType='circle' iconSize={8} verticalAlign='top' align='right' height={24} width={277} />
+        <Bar dataKey="kilogram" name="Poids (kg)" fill="#282D30" barSize={10} radius= {[5, 5, 0, 0]} />
+        <Bar dataKey="calories" name="Calories brulées (kCal)" fill="#E60000" barSize={10} radius= {[5, 5, 0, 0]} />               
       </BarChart>
-    </ResponsiveContainer>
+      <p className='barChartText'>Activité quotidienne</p>
+    </div>
   )  
 }
 
