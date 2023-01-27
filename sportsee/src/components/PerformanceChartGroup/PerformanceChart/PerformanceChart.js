94eaa432ad70fcc5c4202e5be6a264266c466cbf
Veronica <veronica.stevania@gmail.com>
2023-01-27 11:26:30 +0100

    Fix the css on Performance Chart

--- sportsee/src/components/PerformanceChartGroup/PerformanceChart/PerformanceChart.js
+++ sportsee/src/components/PerformanceChartGroup/PerformanceChart/PerformanceChart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 
 import '../PerformanceChart/PerformanceChart.css'
@@ -26,15 +27,26 @@ const PerformanceChart = ({datas}) => {
     return (
         <div className='performance-chart' >
             <ResponsiveContainer width="100%" height="100%">
-                <RadarChart cx="50%" cy="50%" outerRadius="80%" data={performances}>
-                    <PolarGrid />
-                    <PolarAngleAxis dataKey="subject" />
-                    <PolarRadiusAxis />
+                <RadarChart innerRadius="5%" outerRadius="50%" 
+                            data={performances} 
+                            startAngle={-150} endAngle={210}>
+                    <PolarGrid radialLines={false} />
+                    <PolarAngleAxis tickLine={false} tickSize={13}
+                                dy={3} tick={{ fill: "#FFFFFF", fontSize: "12px", fontWeight: "500" }} 
+                                dataKey="subject" />
+                    <PolarRadiusAxis axisLine={false} tickCount={5} tick={false} />
                     <Radar dataKey="value" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
                 </RadarChart>
             </ResponsiveContainer>
         </div>
     )
 }
+
+PerformanceChart.propTypes = {
+    /**
+     * Array containing objects with data values and french labels of each perfomance's category
+     */
+    dataPerf: PropTypes.array,
+  }
 
 export default PerformanceChart
