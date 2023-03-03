962cebad60473fa59f52f5ba649b64eefd95c306
Veronica <veronica.stevania@gmail.com>
2023-03-03 23:24:00 +0100

    Add a mock data and add a constant isMock to switch between API call and mock call

--- sportsee/src/service/Service.js
+++ sportsee/src/service/Service.js
@@ -5,6 +5,22 @@ import { AverageModel } from '../models/AverageModel'
 import { PerformanceModel } from '../models/PerformanceModel'
 
 /**
+ * Mock data from dataMocker.js
+ */
+const {
+    USER_MAIN_DATA,
+    USER_ACTIVITY,
+    USER_AVERAGE_SESSIONS,
+    USER_PERFORMANCE
+} = require('../data/dataMocker')
+
+/**
+ * A setting to switch between mock data or API data
+ * If true then get data from DataMocker.js otherwise get data from API
+ */
+export const isMock = true
+
+/**
  * Get the user's data from the API
  * @param {string} id User's id
  * @returns User's data
@@ -15,14 +31,20 @@ import { PerformanceModel } from '../models/PerformanceModel'
     let errorMessage = null
 
     if(id != null && id !== '') {
-        const baseUrlUser = `http://localhost:3000/user/${id}`
 
-        await axios.get(baseUrlUser)
-        .then((response) => {
-            user = new UserModel(response.data.data)            
-        }).catch((error) => {    
-            errorMessage = error.message
-        })
+        if(isMock === true){
+            let userFound = USER_MAIN_DATA.find((user) => user.id === parseInt(id, 10))
+            user = new UserModel(userFound)
+        } else {
+            const baseUrlUser = `http://localhost:3000/user/${id}`
+
+            await axios.get(baseUrlUser)
+            .then((response) => {
+                user = new UserModel(response.data.data)            
+            }).catch((error) => {    
+                errorMessage = error.message
+            })
+        }
     }
     return { user, errorMessage }
 }
@@ -38,14 +60,19 @@ export const getUserActivity = async (id) => {
     let errorMessage = null
 
     if(id != null && id !== '') {
-        const baseUrlUser = `http://localhost:3000/user/${id}/activity`
+        if(isMock === true){
+            let activityFound = USER_ACTIVITY.find((activity) => activity.userId === parseInt(id, 10))
+            activity = new ActivityModel(activityFound)
+        } else {
+            const baseUrlUser = `http://localhost:3000/user/${id}/activity`
 
-        await axios.get(baseUrlUser)
-        .then((response) => {
-            activity = new ActivityModel(response.data.data)
-        }).catch((error) => {                    
-            errorMessage = error.message
-        })
+            await axios.get(baseUrlUser)
+            .then((response) => {
+                activity = new ActivityModel(response.data.data)
+            }).catch((error) => {                    
+                errorMessage = error.message
+            })
+        }
     }
     return { activity, errorMessage }
 }
@@ -61,14 +88,19 @@ export const getUserAverage = async (id) => {
     let errorMessage = null
 
     if(id != null && id !== '') {
-        const baseUrlUser = `http://localhost:3000/user/${id}/average-sessions`
+        if(isMock === true){
+            let averageFound = USER_AVERAGE_SESSIONS.find((average) => average.userId === parseInt(id, 10))
+            average = new ActivityModel(averageFound)
+        } else {
+            const baseUrlUser = `http://localhost:3000/user/${id}/average-sessions`
 
-        await axios.get(baseUrlUser)
-        .then((response) => {
-            average = new AverageModel(response.data.data)
-        }).catch((error) => {                    
-            errorMessage = error.message
-        })
+            await axios.get(baseUrlUser)
+            .then((response) => {
+                average = new AverageModel(response.data.data)
+            }).catch((error) => {                    
+                errorMessage = error.message
+            })
+        }
     }
     return { average, errorMessage }
 }
@@ -84,14 +116,19 @@ export const getUserPerformance = async (id) => {
     let errorMessage = null
 
     if(id != null && id !== '') {
-        const baseUrlUser = `http://localhost:3000/user/${id}/performance`
+        if(isMock === true){
+            let performanceFound = USER_PERFORMANCE.find((performance) => performance.userId === parseInt(id, 10))
+            performance = new PerformanceModel(performanceFound)
+        } else {
+            const baseUrlUser = `http://localhost:3000/user/${id}/performance`
 
-        await axios.get(baseUrlUser)
-        .then((response) => {
-            performance = new PerformanceModel(response.data.data)
-        }).catch((error) => {                    
-            errorMessage = error.message
-        })
+            await axios.get(baseUrlUser)
+            .then((response) => {
+                performance = new PerformanceModel(response.data.data)
+            }).catch((error) => {                    
+                errorMessage = error.message
+            })
+        }
     }
     return { performance, errorMessage }
 }
